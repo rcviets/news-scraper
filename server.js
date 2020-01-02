a4894d32db5b2d1fa0c21d9545cdb1d63972a2c2
Randamus <rcviets@gmail.com>
2020-01-01 20:36:28 -0700

    server setup

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 //Required Packages
 const mongoose = require("mongoose");
+const express = require("express");
+const logger = require("morgan");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const db = require("./models");
+
+//Set PORT
+const PORT = 3000;
+
+//Initialize Express
+const app = express();
+
+//Middleware
+
+//Logging Requests
+app.use(logger("dev"));
+//Parse Body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//Static Folder = Public
+app.use(express.stating("public"));
 
 //Use Deployed Database or Local Database
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
